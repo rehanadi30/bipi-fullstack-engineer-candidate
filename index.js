078373349161eab2ee81abb17e2cc946c1bb980c
Rehan Adi Satrya <rehanadi457@gmail.com>
2022-12-17 01:56:01 +0700

    graphql can be accessed

--- index.js
+++ index.js
@@ -8,21 +8,47 @@ var { buildSchema } = require('graphql');
 // GraphQL schema
 var schema = buildSchema(`
     type Query {
-        message: String
+        merchant(id: Int!): Merchant
+        merchants(merchant_name:String): [Merchant]
     },
     type Merchant {
         id: Int
         merchant_name: String
-        phoneNumber: String
+        phone_number: String
         latitude : Float
         longitude : Float
-        isActive: Boolean
-        recordedDateTime : String
+        is_active: Boolean
+        recorded_date_time : String
     }
 `);
 
+var merchantsData = [
+    {
+        id: 99,
+        merchant_name: 'duar headshot',
+        phone_number: '+62213213123213',
+        longitude: 103.2,
+        latitude: -6.4,
+        is_active: false,
+        recorded_date_time: '2022-12-16 07:00:00.000 +0700'
+    }
+]
+
+var getMerchant = function(args) {
+    var id = args.id;
+    return merchantsData.filter(course => {
+        return course.id == id;
+    })[0];
+}
+
+var getMerchants = function(args) {
+    return merchantsData;
+}
+
+
 var root = {
-    message: () => 'Hello World!'
+    merchant: getMerchant,
+    merchants: getMerchants
 }
 
 const knex = require('knex')({
